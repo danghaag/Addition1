ebf28f7dc61507619014a6df98829316c23638c8
Daniel Haag <danghaag99@att.net>
2014-02-07 14:11:51 -0600

    Addition - C# test

---
+++ Addition1/Addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Addition
+{
+    public class Addition
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int sum;
+
+            Console.Write("Enter first integer: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = number1 + number2;
+            Console.WriteLine( "Sum is {0}", sum );
+        }
+    }
+}
